8eafa3acd9a781e80ed7e7ccbd845dbcec964c84
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-05-15 18:10:34 +0300

    fix: fix issue with setup complete page

--- src/actions/setup-account.ts
+++ src/actions/setup-account.ts
@@ -47,7 +47,7 @@ export const setupAccount = async (
 
         const successMessage = stage == 'one' ? 'Account setup 1/3' : 'Account setup 3/3';
 
-        const redirectUrl = stage !== 'three' ? `/auth/setup-account?stage=${stage == 'one' ? 'two' : 'three'}` : '/auth/setup-complete';
+        const redirectUrl = stage !== 'three' ? `/auth/setup-account?stage=${stage == 'one' ? 'two' : 'three'}` : `/auth/setup-complete?email=${formData.get('email')}`;
 
         return toFormState('INFO', successMessage, redirectUrl);
     } catch (error) {
--- src/app/auth/setup-complete/page.tsx
+++ src/app/auth/setup-complete/page.tsx
@@ -1,7 +1,16 @@
 'use client'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-function SetupComplete() {
+type Props = {
+  searchParams: {
+    email: string
+  }
+}
+
+function SetupComplete({ searchParams: { email } }: Props) {
+
+  if (!email) notFound()
 
   return (
     <div className='text-black w-9/12 flex flex-col gap-5 items-center'>
@@ -11,7 +20,7 @@ function SetupComplete() {
       <div className=''>
         <p>
           The notification about passing moderation on the platform will be sent to
-          &nbsp; <span className='text-primary font-semibold'>johnsmith@gmail.com.</span>
+          &nbsp; <span className='text-primary font-semibold'>{email}.</span>
           &nbsp; <span className='text-primary font-semibold'>It could take up to 48 hours.</span>
         </p>
       </div>
