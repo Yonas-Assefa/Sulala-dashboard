c6a6578cc37972f0eaea3854d0249729a38afdf0
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-05-27 15:38:34 +0300

    feat: make setting tab responsive feat: add responsiveness to navigation bar

--- src/app/dashboard/components/SideBarNav.tsx
+++ src/app/dashboard/components/SideBarNav.tsx
@@ -10,10 +10,20 @@ function SideBarNav() {
     const pathname = usePathname()
 
     return (
-        <nav className='min-w-[300px] h-screen bg-tertiary flex flex-col justify-between'>
+        <nav className='md:min-w-[300px] z-50 drop-shadow-lg group md:h-full bg-tertiary fixed md:relative md:w-auto w-full h-auto md:flex flex-col justify-between'>
             <div>
-                <SulalaLogo />
-                <ul className="flex flex-col list-none">
+                <div className='flex flex-row justify-between items-center'>
+                    <SulalaLogo />
+                    <div className='m-2 p-1 md:hidden bg-primary rounded-sm'>
+                        <input type="checkbox" className='peer' hidden name="nav_bar" id="nav_bar" />
+                        <label htmlFor="nav_bar" className=' p-1 flex flex-col gap-1'>
+                            <div className="bg-tertiary h-[2px] w-[25px] group-has-[:checked]:rotate-45 group-has-[:checked]:translate-y-[6px] transition-all" />
+                            <div className="bg-tertiary h-[2px] w-[25px] group-has-[:checked]:invisible" />
+                            <div className="bg-tertiary h-[2px] w-[25px] group-has-[:checked]:-rotate-45 group-has-[:checked]:-translate-y-[6px] transition-all" />
+                        </label>
+                    </div>
+                </div>
+                <ul className="group-has-[:checked]:flex hidden transition-all md:flex flex-col list-none peer-has-[checked]:hidden">
                     {
                         routes.map((route) => {
                             const isActive = pathname.includes(route.path.split('?')[0])
@@ -31,7 +41,7 @@ function SideBarNav() {
                     }
                 </ul>
             </div>
-            <div className='m-6 flex flex-row gap-3 items-center text-black font-semibold'>
+            <div className='m-6 hidden md:flex flex-row gap-3 items-center text-black font-semibold'>
                 <img src="/icons/whatsup-logo.svg" alt="whatsup icon" className='h-[30px] aspect-square' />
                 <Link href={'/support/contact'}>Contact support</Link>
             </div>
