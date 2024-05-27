76964d0023af77ed2eaf137e5ce4fa1f4963caed
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-05-27 14:29:08 +0300

    feat: add responsiveness to setup complete page

--- src/app/auth/setup-complete/page.tsx
+++ src/app/auth/setup-complete/page.tsx
@@ -22,9 +22,9 @@ function SetupComplete({ searchParams: { email } }: Props) {
   }, [])
 
   return (
-    <div className='text-black w-9/12 flex flex-col gap-5 items-center'>
+    <div className='text-black w-11/12 md:w-9/12 flex flex-col gap-6 items-center'>
       {/* SIGN IN HEADER */}
-      <h1 className='text-[50px] font-serif font-semibold self-start'>Thank you!</h1>
+      <h1 className='text-3xl md:text-[40px] font-serif font-semibold self-start'>Thank you!</h1>
 
       <div className=''>
         <p>
