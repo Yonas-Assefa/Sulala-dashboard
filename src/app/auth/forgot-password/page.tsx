16e96b8913101a6a5f7c080c23eec63a6ce403a1
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-05-27 14:21:20 +0300

    make password forgot page responsive

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -19,11 +19,11 @@ function ForgotPassword() {
     return (
         <div className='text-black w-10/12 flex flex-col gap-5 items-center'>
             {/* SIGN IN HEADER */}
-            <h1 className='text-[50px] font-serif font-semibold'>Forgot password</h1>
+            <h1 className='text-3xl md:text-[45px] font-serif font-semibold'>Forgot password</h1>
             <div className='w-2/3'>
                 <p className='text-center text-primary font-semibold text-sm'>Enter your email address to proceed with password reset</p>
             </div>
-            <form action={action} className='flex flex-col gap-6 w-full px-10'>
+            <form action={action} className='flex flex-col gap-6 w-full md:px-10'>
                 {/* SIGN IN INPUT */}
                 <AuthWithEmail takePassword={false} emailError={formState?.fieldErrors?.email?.[0]} />
                 <div className='flex flex-col gap-3 w-full items-center'>
