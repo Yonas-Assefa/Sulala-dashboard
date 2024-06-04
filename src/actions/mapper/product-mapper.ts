87bd11e8ac12a0a9ea742a1320c31fa10bee76a5
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-04 14:27:21 +0300

    merge product mapper

--- src/actions/mapper/product-mapper.ts
+++ src/actions/mapper/product-mapper.ts
@@ -1,32 +1,36 @@
-import { getCategories } from "../common/get-categories"
-import { BASE_URL } from "../../config/urls"
-import { constructImageUrl, deconstructImageUrl } from "@/lib/images"
+import { getCategories } from "../common/get-categories";
+import { BASE_URL } from "../../config/urls";
+import { constructImageUrl, deconstructImageUrl } from "@/lib/images";
 
 export const productMapper = async (data: any) => {
-    const categories = await getCategories()
+  const categories = await getCategories();
 
-    function convert(item: any) {
-        return {
-            ...item,
-            category: getSubCategory(categories, item.category)?.label,
-            category_value: getSubCategory(categories, item.category)?.value,
-            tags: item.tags.map((tag: any) => ({ label: tag.name, value: tag.id })),
-            images: constructImageUrl(item.images?.[0], true),
-            deconstructed_images: deconstructImageUrl(item.images?.[0])
-        }
-    }
+  function convert(item: any) {
+    return {
+      ...item,
+      category: getSubCategory(categories, item.category)?.label,
+      category_value: getSubCategory(categories, item.category)?.value,
+      tags: item.tags.map((tag: any) => ({ label: tag.name, value: tag.id })),
+      images: constructImageUrl(item.images?.[0], true),
+      deconstructed_images: deconstructImageUrl(item.images?.[0]),
+    };
+  }
 
-    if (Array.isArray(data)) {
-        return data.map((product: any) => {
-            return convert(product)
-        })
-    } else {
-        return convert(data)
-    }
-}
+  if (Array.isArray(data)) {
+    return data.map((product: any) => {
+      return convert(product);
+    });
+  } else {
+    return convert(data);
+  }
+};
 
 const getSubCategory = (categories: any, id: number) => {
-    const category = categories.find((category: any) => (category.options.map((o: any) => o.value)).includes(id))
-    const subCategory = category ? category.options.find((o: any) => o.value === id) : null
-    return subCategory || category || { label: 'No category', value: 0 }
-}
+  const category = categories.find((category: any) =>
+    category.options.map((o: any) => o.value).includes(id)
+  );
+  const subCategory = category
+    ? category.options.find((o: any) => o.value === id)
+    : null;
+  return subCategory || category || { label: "No category", value: 0 };
+};
--- src/actions/mapper/shop-mapper.ts
+++ src/actions/mapper/shop-mapper.ts
@@ -1,22 +1,29 @@
-import { getCategories } from "../common/get-categories"
-import { BASE_URL } from "../../config/urls"
-import { constructImageUrl, deconstructImageUrl } from "@/lib/images"
+import { getCategories } from "../common/get-categories";
+import { BASE_URL } from "../../config/urls";
 
 export const shopMapper = async (data: any) => {
-    const categories = await getCategories()
-    function convert(item: any) {
-        return {
-            ...item,
-            profile_photo: constructImageUrl(item.profile_photo, true),
-            deconstructed_profile_photo: deconstructImageUrl(item.profile_photo),
-            category: categories.find((category) => category.value === item.category)
-        }
-    }
-    if (Array.isArray(data)) {
-        return data.map((billing: any) => {
-            return convert(billing)
-        })
-    } else {
-        return convert(data)
-    }
-}
+  const categories = await getCategories();
+  if (Array.isArray(data)) {
+    return data.map((billing: any) => {
+      return {
+        ...billing,
+        profile_photo:
+          billing.profile_photo?.length > 0
+            ? `${BASE_URL}${billing.profile_photo}`
+            : undefined,
+        category: categories.find(
+          (category) => category.value === billing.category
+        ),
+      };
+    });
+  } else {
+    return {
+      ...data,
+      profile_photo:
+        data.profile_photo?.length > 0
+          ? `${BASE_URL}${data.profile_photo}`
+          : undefined,
+      category: categories.find((category) => category.value === data.category),
+    };
+  }
+};
