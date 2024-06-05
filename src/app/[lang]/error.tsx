1257f37f6ec4e1d2f41dafb701b5e1e7f3f976ed
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-05 16:07:31 +0300

    fix error page issue

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -9,11 +9,11 @@ function error() {
             <div className='bg-white flex-grow'>
                 <div className='flex flex-col justify-center items-center w-full h-full'>
                     <img src="/icons/alert.svg" alt="" className='opacity-95' />
-                    <h1 className='text-xl md:text-2xl text-center font-semibold text-danger'>{t('trouble_loading_page')} {':('}</h1>
+                    <h1 className='text-xl md:text-2xl text-center font-semibold text-danger p-3 md:p-1'>{t('trouble_loading_page')} {':('}</h1>
                     <div className='flex flex-row justify-between gap-5'>
-                        <p className='text-primary font-semibold'>{t('we_are_working_on_it')}</p>
+                        <p className='text-primary font-semibold p-3 md:p-1'>{t('we_are_working_on_it')}</p>
                     </div>
-                    <p className='text-secondary font-semibold'>{t('try_refreshing_the_page')}</p>
+                    <p className='text-secondary font-semibold p-3 md:p-1'>{t('try_refreshing_the_page')}</p>
                     <img src='/sulala-logo.svg' className=' w-[100px] opacity-15 absolute' />
                 </div>
             </div>
