d2a915938ce5651724d0282b3093f1fa0042ff17
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-06 15:14:39 +0300

    translate modals

--- src/app/[lang]/dashboard/settings/components/modals/ChangePasswordModal.tsx
+++ src/app/[lang]/dashboard/settings/components/modals/ChangePasswordModal.tsx
@@ -9,6 +9,7 @@ import { EMPTY_FORM_STATE } from '@/utils/formStateHelper';
 import React, { useEffect } from 'react'
 import { useDetectClickOutside } from 'react-detect-click-outside';
 import { useFormState } from 'react-dom';
+import { useTranslations } from 'next-intl';
 
 function ChangePasswordModal() {
     const [formState, action] = useFormState(
@@ -18,6 +19,8 @@ function ChangePasswordModal() {
 
     useToastMessage(formState);
     useRedirectRoute(formState);
+
+    const t = useTranslations('Settings.PersonalInfo')
 
     useEffect(() => {
         if (formState.status === 'SUCCESS') {
@@ -29,13 +32,13 @@ function ChangePasswordModal() {
         <dialog id="change_password_setting_modal" className='modal'>
             <form action={action} className="modal-box w-11/12 max-w-sm bg-white px-0">
                 <div className='border-b-2 border-gray-200 pb-3'>
-                    <h3 className="font-bold text-xl text-black text-center font-serif">Change Password</h3>
+                    <h3 className="font-bold text-xl text-black text-center font-serif">{t('change_password')}</h3>
                 </div>
                 <div className="px-5 flex flex-col gap-3 mt-4">
-                    <PasswordInput id='old_password' label='Current password' name='old_password' showLabel={false} placeholder='Enter current password' error={formState?.fieldErrors?.old_password?.[0]} />
-                    <PasswordInput id='new_password' label='New password' name='new_password' showLabel={false} placeholder='Enter new password' error={formState?.fieldErrors?.password?.[0]} />
-                    <PasswordInput id='confirm_password' label='Confirm password' name='confirm_password' showLabel={false} placeholder='Cofirm password' error={formState?.fieldErrors?.confirm_password?.[0]} />
-                    <PrimaryButton name='Confirm' type='submit' />
+                    <PasswordInput id='old_password' label='Current password' name='old_password' showLabel={false} placeholder={t('enter_current_password')} error={formState?.fieldErrors?.old_password?.[0]} />
+                    <PasswordInput id='new_password' label='New password' name='new_password' showLabel={false} placeholder={t('enter_new_password')} error={formState?.fieldErrors?.password?.[0]} />
+                    <PasswordInput id='confirm_password' label='Confirm password' name='confirm_password' showLabel={false} placeholder={t('confirm_new_password')} error={formState?.fieldErrors?.confirm_password?.[0]} />
+                    <PrimaryButton name={t('confirm')} type='submit' />
                 </div>
             </form>
             <form method="dialog" className="modal-backdrop">
