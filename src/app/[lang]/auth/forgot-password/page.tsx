be1bfa885d4349d22303b4ef106b14d68dae2a58
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-06 14:17:50 +0300

    forgot password translation

--- src/app/[lang]/auth/forgot-password/page.tsx
+++ src/app/[lang]/auth/forgot-password/page.tsx
@@ -8,6 +8,7 @@ import { useToastMessage } from '@/hooks/useToastMessage'
 import { EMPTY_FORM_STATE } from '@/utils/formStateHelper'
 import React, { useActionState } from 'react'
 import { useFormState } from 'react-dom'
+import { useTranslations } from 'next-intl'
 
 function ForgotPassword() {
 
@@ -16,24 +17,26 @@ function ForgotPassword() {
     useToastMessage(formState)
     useRedirectRoute(formState)
 
+    const t = useTranslations('Auth')
+
     return (
         <div className='text-black w-10/12 flex flex-col gap-5 items-center'>
             {/* SIGN IN HEADER */}
-            <h1 className='text-3xl md:text-[45px] font-serif font-semibold'>Forgot password</h1>
+            <h1 className='text-3xl md:text-[45px] font-serif font-semibold'>{t('forgot_password')}</h1>
             <div className='w-2/3'>
-                <p className='text-center text-primary font-semibold text-sm'>Enter your email address to proceed with password reset</p>
+                <p className='text-center text-primary font-semibold text-sm'>{t('enter_email_to_proceed')}</p>
             </div>
             <form action={action} className='flex flex-col gap-6 w-full md:px-10'>
                 {/* SIGN IN INPUT */}
                 <AuthWithEmail takePassword={false} emailError={formState?.fieldErrors?.email?.[0]} />
                 <div className='flex flex-col gap-3 w-full items-center'>
                     <div className='w-full flex flex-col'>
-                        <PrimaryButton name='Continue' type='submit' />
+                        <PrimaryButton name={t('continue')} type='submit' />
                     </div>
 
 
-                    <p className='text-[#70757f]'>Already have an account?</p>
-                    <SecondaryButton name='Sign in' href={'/auth/sign-in'} />
+                    <p className='text-[#70757f]'>{t('already_have_an_account')}</p>
+                    <SecondaryButton name={t('signin')} href={'/auth/sign-in'} />
 
                 </div>
             </form>
