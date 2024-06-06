35cdf24d7610acf7fe827396003c7595893bb87c
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-06 14:48:07 +0300

    setup account translated

--- src/app/[lang]/auth/setup-complete/page.tsx
+++ src/app/[lang]/auth/setup-complete/page.tsx
@@ -4,6 +4,7 @@ import { useSetupAccountStore } from '@/providers/setup-account-store-provider'
 import { useRouter } from '@/i18n/navigation'
 import React from 'react'
 import { notFound } from 'next/navigation'
+import { useTranslations } from 'next-intl'
 
 type Props = {
   searchParams: {
@@ -18,16 +19,17 @@ function SetupComplete({ searchParams: { email } }: Props) {
     clearCookie()
   }, [])
 
+  const t = useTranslations('Auth')
+
   return (
     <div className='text-black w-11/12 md:w-9/12 flex flex-col gap-6 items-center'>
       {/* SIGN IN HEADER */}
-      <h1 className='text-3xl md:text-[40px] font-serif font-semibold self-start'>Thank you!</h1>
+      <h1 className='text-3xl md:text-[40px] font-serif font-semibold self-start'>{t('thank_you')}</h1>
 
       <div className=''>
         <p>
-          The notification about passing moderation on the platform will be sent to
-          &nbsp; <span className='text-primary font-semibold'>{email}.</span>
-          &nbsp; <span className='text-primary font-semibold'>It could take up to 48 hours.</span>
+          {t('moderation_notification_will_be_sent', { email })}
+          &nbsp; <span className='text-primary font-semibold'>{t('it_will_take_48_hr')}</span>
         </p>
       </div>
 
