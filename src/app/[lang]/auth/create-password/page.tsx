abb8cf7d9fea10bddedb7701724c3b1693f64f59
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-07 13:45:07 +0300

    clean up

--- src/app/[lang]/auth/create-password/page.tsx
+++ src/app/[lang]/auth/create-password/page.tsx
@@ -1,28 +1,9 @@
-import { getPersonalInfo } from '@/actions/settings/get-personal-info'
 import CreatePasswordForm from './CreatePasswordForm'
 import React from 'react'
-import { redirect } from '@/i18n/navigation'
 import { getTranslations } from 'next-intl/server'
 
 async function CreatePassword() {
-
-  const isError = (val: unknown): val is Error => {
-    if (val && typeof val == 'object' && 'message' in val && typeof val.message == 'string') return true
-    return false
-  }
-
   const t = await getTranslations('Auth')
-
-  try {
-    const personalInfo = await getPersonalInfo()
-    if (personalInfo?.is_password_set) {
-      redirect('/dashboard/settings')
-    }
-  } catch (error: unknown) {
-    if (isError(error) && error.message.includes('An authentication token is not provided or invalid')) {
-      redirect('/auth/sign-in')
-    }
-  }
 
   return (
     <div className='text-black w-10/12 flex flex-col gap-8 items-center'>
