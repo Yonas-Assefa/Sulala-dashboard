3b70324e5f0da319bc2957741acbcf3fc0cc21ee
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-11 16:36:19 +0300

    feat: add resend password create link and setup password create ui
    feat: integrate resend password create link
    
    feat: created new modal for resent create passord link

--- src/actions/auth/resend-create-password-link.ts
+++ src/actions/auth/resend-create-password-link.ts
@@ -1,28 +1,43 @@
-'use server'
-import { fromErrorToFormState, toFormState } from '@/utils/formStateHelper';
-import { RESEND_VERIFICATION_LINK } from '../../config/urls';
-import { emailSignUpSchema } from '../schema/zod-schema';
-import { cookies } from 'next/headers';
-import { getResponseBody, getResponseErrorMessage, makeRequest, setBrowserCookie } from '../../lib/helper';
+"use server";
+import { fromErrorToFormState, toFormState } from "@/utils/formStateHelper";
+import { RESEND_PASSWORD_SETUP_LINK } from "../../config/urls";
+import { emailSignUpSchema } from "../schema/zod-schema";
+import { cookies } from "next/headers";
+import {
+  getResponseBody,
+  getResponseErrorMessage,
+  makeRequest,
+  setBrowserCookie,
+} from "../../lib/helper";
 
-export const resendCreatePasswordLink = async ({ email }: { email: string }) => {
-    try {
-        const data = emailSignUpSchema.parse({
-            email,
-        });
+export const resendCreatePasswordLink = async ({
+  email,
+}: {
+  email: string;
+}) => {
+  try {
+    const data = emailSignUpSchema.parse({
+      email,
+    });
 
-        const response = await makeRequest(RESEND_VERIFICATION_LINK, data, 'PATCH')
+    const response = await makeRequest(
+      RESEND_PASSWORD_SETUP_LINK,
+      data,
+      "POST",
+    );
 
-        const body = await getResponseBody(response)
+    const body = await getResponseBody(response);
 
-        if (!response.ok || !body.success) {
-            throw new Error(getResponseErrorMessage(body) || 'Failed to resend verification link');
-        }
-
-        const successMessage = 'Verification link resent successfully!'
-
-        return toFormState('SUCCESS', successMessage);
-    } catch (error) {
-        return fromErrorToFormState(error);
+    if (!response.ok || !body.success) {
+      throw new Error(
+        getResponseErrorMessage(body) || "Failed to resend verification link",
+      );
     }
+
+    const successMessage = "Verification link resent successfully!";
+
+    return toFormState("SUCCESS", successMessage);
+  } catch (error) {
+    return fromErrorToFormState(error);
+  }
 };
