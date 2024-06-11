a9aa2188708ff93e088968eba1be052f2a0a3933
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-11 17:11:40 +0300

    feat: verify setup passord

--- src/actions/auth/resend-create-password-link.ts
+++ src/actions/auth/resend-create-password-link.ts
@@ -2,12 +2,11 @@
 import { fromErrorToFormState, toFormState } from "@/utils/formStateHelper";
 import { RESEND_PASSWORD_SETUP_LINK } from "../../config/urls";
 import { emailSignUpSchema } from "../schema/zod-schema";
-import { cookies } from "next/headers";
+
 import {
   getResponseBody,
   getResponseErrorMessage,
   makeRequest,
-  setBrowserCookie,
 } from "../../lib/helper";
 
 export const resendCreatePasswordLink = async ({
--- src/actions/auth/verify-create-password.ts
+++ src/actions/auth/verify-create-password.ts
@@ -1,32 +1,45 @@
-'use server'
-import { fromErrorToFormState, toFormState } from '@/utils/formStateHelper';
-import { VERIFY_EMAIL } from '../../config/urls';
-import { getResponseBody, getResponseErrorMessage, makeRequest, setBrowserCookie } from '../../lib/helper';
+"use server";
+import { fromErrorToFormState, toFormState } from "@/utils/formStateHelper";
+import { CONFIRM_PASSWORD_SETUP } from "../../config/urls";
+import {
+  getResponseBody,
+  getResponseErrorMessage,
+  makeRequest,
+  setBrowserCookie,
+} from "../../lib/helper";
 
 type VerifyEmailArgs = {
-    vendor_id: string
-    reset_link: string
-}
+  vendor_id: string;
+  reset_link: string;
+};
 
-export const verifyCreatepassword = async ({ reset_link: confirmation_token, vendor_id }: VerifyEmailArgs) => {
-    try {
-        const response = await makeRequest(`${VERIFY_EMAIL}?confirmation_token=${confirmation_token}&vendor_id=${vendor_id}`, {}, 'GET')
-        const body = await getResponseBody(response)
+export const verifyCreatepassword = async ({
+  reset_link: confirmation_token,
+  vendor_id,
+}: VerifyEmailArgs) => {
+  try {
+    const response = await makeRequest(
+      `${CONFIRM_PASSWORD_SETUP}?confirmation_token=${confirmation_token}&vendor_id=${vendor_id}`,
+      {},
+      "GET",
+    );
+    const body = await getResponseBody(response);
 
-        if (!response.ok || !body.success) {
-            const redirectUrl = '/auth/sign-in'
-            const failedMessage = getResponseErrorMessage(body) || 'Failed to verify email address'
-            return toFormState('ERROR', failedMessage, redirectUrl);
-        }
-
-        setBrowserCookie(response)
-
-        const successMessage = 'Verification successful!'
-
-        const redirectUrl = '/auth/create-password'
-
-        return toFormState('SUCCESS', successMessage, redirectUrl);
-    } catch (error) {
-        return fromErrorToFormState(error);
+    if (!response.ok || !body.success) {
+      const redirectUrl = "/auth/sign-in";
+      const failedMessage =
+        getResponseErrorMessage(body) || "Failed to verify email address";
+      return toFormState("ERROR", failedMessage, redirectUrl);
     }
+
+    setBrowserCookie(response);
+
+    const successMessage = "Verification successful!";
+
+    const redirectUrl = "/auth/create-password";
+
+    return toFormState("SUCCESS", successMessage, redirectUrl);
+  } catch (error) {
+    return fromErrorToFormState(error);
+  }
 };
