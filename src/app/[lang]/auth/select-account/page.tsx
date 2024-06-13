899b9711452d3ec73c4ef6f2fcff6748130fd035
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-13 13:18:09 +0300

    feat: add icons for select account

--- src/app/[lang]/auth/select-account/page.tsx
+++ src/app/[lang]/auth/select-account/page.tsx
@@ -20,11 +20,22 @@ async function SelectAccount({ searchParams: { action } }: Props) {
         <div className="flex">
           <div className="card drop-shadow-lg flex flex-col gap-2">
             {[
-              { label: "Farmer Account", value: "farmer" },
-              { label: "Customer Account", value: "customer" },
-              { label: "Vendor Account", value: "vendor" },
+              {
+                label: "Farmer Account",
+                value: "farmer",
+                description:
+                  "showcase their livestock, crops, and agricultural products, connecting them with potential buyers and facilitating business growth in the Middle Eastern market.",
+                image: "/icons/app-and-play-store.png",
+              },
+              {
+                label: "Vendor Account",
+                value: "vendor",
+                description:
+                  "empowers sellers of equipment, tools, foods, and related products to reach a wide audience of animal enthusiasts and professionals in the Middle East",
+                image: "/sulala-logo.svg",
+              },
             ].map((item, i) => (
-              <div className="collapse bg-white" key={item.value}>
+              <div className="collapse bg-white relative" key={item.value}>
                 <input
                   type="radio"
                   name="account_type"
@@ -32,14 +43,16 @@ async function SelectAccount({ searchParams: { action } }: Props) {
                   className="peer"
                   defaultChecked={i == 0}
                 />
-                <div className="collapse-title text-xl bg-tertiary font-medium peer-checked:bg-white peer-checked:text-primary peer-checked:font-bold transition-all">
-                  {item.label}
+                <div className="collapse-title flex justify-between text-xl bg-tertiary font-medium peer-checked:bg-white peer-checked:text-primary peer-checked:font-bold transition-all">
+                  <h2>{item.label}</h2>
                 </div>
+                <img
+                  src={item.image}
+                  alt=""
+                  className="h-[20px] invisible peer-checked:visible absolute right-3 top-3"
+                />
                 <div className="collapse-content peer-checked:bg-gradient-to-b from-primary/0 to-primary/10">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quae, dignissimos.
-                  </p>
+                  <p>{item.description}</p>
                 </div>
               </div>
             ))}
@@ -47,7 +60,7 @@ async function SelectAccount({ searchParams: { action } }: Props) {
         </div>
 
         <div className="flex justify-center w-full">
-          <PrimaryButton name="Continue" padding="sm" />
+          <PrimaryButton name="Continue" padding="sm" href="/auth/sign-up" />
         </div>
       </form>
     </div>
