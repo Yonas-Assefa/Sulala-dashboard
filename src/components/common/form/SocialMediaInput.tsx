6e4692b7f534fdf537a278bd4bfb26cfebb0b612
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-06-18 12:01:04 +0300

    style: fix style issue with social media input

--- src/components/common/form/SocialMediaInput.tsx
+++ src/components/common/form/SocialMediaInput.tsx
@@ -1,25 +1,41 @@
-import { useTranslations } from 'next-intl'
-import React from 'react'
+import { useTranslations } from "next-intl";
+import React from "react";
 
 type Props = {
-    socialMedia: 'facebook' | 'instagram'
-    error?: string | undefined
-    defaultValue?: string
-}
+  socialMedia: "facebook" | "instagram";
+  error?: string | undefined;
+  defaultValue?: string;
+};
 function SocialMediaInput({ socialMedia, error, defaultValue }: Props) {
-    const t = useTranslations('Commons');
-    return (
-        <div>
-            <label htmlFor={socialMedia}>{socialMedia == 'instagram' ? t('instagram_link') : t('facebook_link')}</label>
-            <div className={`flex flex-row gap-2 border-2 rounded-[30px] px-4 ${error ? 'border-danger bg-dangerlight' : 'bg-white'}`}>
-                <img src={socialMedia == 'instagram' ? '/icons/instagram.svg' : '/icons/facebook.svg'} alt="" />
-                <input defaultValue={defaultValue} type="text" id={socialMedia} name={socialMedia} placeholder='Insert link' className='input bg-transparent outline-none active:outline-none active:border-0 focus:outline-none focus:border-0' />
-            </div>
-            {error && <span className="text-xs text-danger">
-                {error}
-            </span>}
-        </div>
-    )
+  const t = useTranslations("Commons");
+  return (
+    <div className="flex flex-col gap-3">
+      <label htmlFor={socialMedia}>
+        {socialMedia == "instagram" ? t("instagram_link") : t("facebook_link")}
+      </label>
+      <div
+        className={`flex flex-row gap-2 border-2 rounded-[30px] px-4 ${error ? "border-danger bg-dangerlight" : "bg-white"}`}
+      >
+        <img
+          src={
+            socialMedia == "instagram"
+              ? "/icons/instagram.svg"
+              : "/icons/facebook.svg"
+          }
+          alt=""
+        />
+        <input
+          defaultValue={defaultValue}
+          type="text"
+          id={socialMedia}
+          name={socialMedia}
+          placeholder="Insert link"
+          className="input bg-transparent outline-none active:outline-none active:border-0 focus:outline-none focus:border-0"
+        />
+      </div>
+      {error && <span className="text-xs text-danger">{error}</span>}
+    </div>
+  );
 }
 
-export default SocialMediaInput
+export default SocialMediaInput;
