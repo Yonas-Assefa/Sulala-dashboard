43114cfc5fcef3b74ea6d269ff40172fccaece8c
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-07-16 15:16:53 +0300

    fix: fix issues with redirect

--- src/hooks/useRedirectRoute.ts
+++ src/hooks/useRedirectRoute.ts
@@ -3,18 +3,19 @@ import { usePathname, useRouter } from "@/i18n/navigation";
 import { useEffect, useRef } from "react";
 
 export const useRedirectRoute = (formState: FormState) => {
-    const router = useRouter();
-    const pathname = usePathname();
-    const prevTimestamp = useRef(formState.timestamp);
+  const router = useRouter();
+  const pathname = usePathname();
+  const prevTimestamp = useRef(formState.timestamp);
 
-    const doRedirect =
-        formState.redirectUrl &&
-        formState.timestamp !== prevTimestamp.current &&
-        pathname !== formState.redirectUrl;
+  const doRedirect =
+    formState.redirectUrl &&
+    formState.timestamp !== prevTimestamp.current &&
+    pathname !== formState.redirectUrl;
 
-    useEffect(() => {
-        if (doRedirect) {
-            router.push(formState.redirectUrl as any);
-        }
-    }, [formState, doRedirect]);
-}
+  useEffect(() => {
+    if (doRedirect) {
+      prevTimestamp.current = formState.timestamp;
+      router.push(formState.redirectUrl as any);
+    }
+  }, [formState, doRedirect]);
+};
