97fccc71101ded45d35510992437b8d573192d4f
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-08-01 13:04:47 +0300

    hotfix: fix push script

--- scripts/docker/PUSH.sh
+++ scripts/docker/PUSH.sh
@@ -1,26 +1,22 @@
 # Get the latest tag of the docker image
 
-label_latest=$1
+# echo -e "\e[0;36;49mPush the current tag with 'latest' tag? (y/n)\e[0m"
+# read label_latest
 
-if [ "$label_latest" != "y" ] && [ "$label_latest" != "n" ]; then
-  echo -e "\e[0;36;49mPush the current tag with 'latest' tag? (y/n)\e[0m"
-  read label_latest
-fi
+# if [ "$label_latest" == "y" ]; then
+#   echo -e "\e[0;32;49m[Yes]\e[0m pushing the current tag with 'latest' tag"
+# else
+#   echo -e "\e[0;31;49m[No]\e[0m skipping to push the 'latest' tag"
+# fi
 
-if [ "$label_latest" == "y" ]; then
-  echo -e "\e[0;32;49m[Yes]\e[0m pushing the current tag with 'latest' tag"
-else
-  echo -e "\e[0;31;49m[No]\e[0m skipping to push the 'latest' tag"
-fi
-
-local label_latest
+# local label_latest
 latest_tag=$(docker images | grep sulala-dashboard | awk '{print $2}' | grep -v latest | grep -v none | sort -r | head -n 1)
 
 # push the docker image to docker hub
 docker push yosephtadesse/sulala-dashboard:$latest_tag
-if [ "$label_latest" == "y" ]; then
+# if [ "$label_latest" == "y" ]; then
   docker push yosephtadesse/sulala-dashboard:latest
-fi
+# fi
 
 echo -e "\e[0;34;49mDocker image pushed to Docker Hub\e[0m"
 echo -e "\e[0;33;49mDocker image tags: yosephtadesse/sulala-dashboard:$latest_tag\e[0m"
